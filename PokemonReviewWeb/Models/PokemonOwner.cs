9141f5dc37a4da274bfae6b0e9962aa70d181632
LOZONSCHI CONSTANTIN-RAZVAN <310440501RSL211122@feaa.uaic.ro>
2023-07-04 09:20:37 +0300

    init

---
+++ PokemonReviewWeb/Models/PokemonOwner.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonReviewWeb.Models
+{
+    public class PokemonOwner
+    {
+        public int PokemonId { get; set; }
+        public string OwnerId { get; set;}
+        public Pokemon Pokemon { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
